ffe48e86a042659c9b4d7710efccf845bc28a423
Arthur Ascedu <arthurascedu@proton.me>
2026-06-02 14:42:08 +0200

    p3 close to the end, missing host connection to leaf

--- P3/router/routerB-network.sh
+++ P3/router/routerB-network.sh
@@ -1,1 +1,9 @@
 #! /bin/bash
+ip link add vxlan10 type vxlan id 10 dev eth0 dstport 4789 local 10.255.0.12 nolearning
+ip link set vxlan10 up
+
+ip link add br-vxlan10 type bridge
+ip link set br-vxlan10 up
+
+ip link set vxlan10 master br-vxlan10
+ip link set eth1 master br-vxlan10
